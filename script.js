129ce6edb6c1fd368ab837cd8a7a6a1f37a095b8
ruhul9990 <mrruhulamin100@gmail.com>
2025-06-11 07:08:38 +0600

    Ss

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function showAlert() {
+    const alertBox = document.getElementById('alertBox');
+    // এখানে লজিক দিয়ে এলার্ট মেসেজ শো করা যাবে
+    alertBox.textContent = "⚡ এখনই কিনুন! দাম উঠছে দ্রুত।";
+}
